6e12dc639e4d7b2b8edd6577308d46c3d8f38a75
Ramprakash Cadirvele <rcadirvele@outlook.com>
2022-03-24 15:50:24 +0530

    Removed/cleaned-up the existing day plan and Newtonsoft.Json reference which causes circular reference error.

--- LOC.PMS.Application/PalletDetailsProvider.cs
+++ LOC.PMS.Application/PalletDetailsProvider.cs
@@ -5,11 +5,10 @@ using LOC.PMS.Application.Interfaces;
 using LOC.PMS.Application.Interfaces.IRepositories;
 using LOC.PMS.Model;
 using Serilog;
-using static System.Threading.Tasks.TaskStatus;
 
 namespace LOC.PMS.Application
 {
-    public class PalletDetailsProvider : IPalletDetailsProvider
+    public class PalletDetailsProvider:IPalletDetailsProvider
     {
         private readonly IPalletRepository _palletRepository;
         private readonly ILogger _logger;
@@ -20,27 +19,6 @@ namespace LOC.PMS.Application
             _logger = logger;
         }
 
-        public async Task AddDayPlanData(List<DayPlan> order)
-        {
-            try
-            {
-                _logger.ForContext("PalletDetailsRequest", order)
-                    .Information("Add Pallet request - Start");
-
-                //business logic
-
-                await _palletRepository.AddDayPlanData(order);
-
-                _logger.ForContext("PalletDetailsRequest", order)
-                    .Information("Add Pallet request - End");
-            }
-            catch (Exception exception)
-            {
-                _logger.ForContext("PalletDetailsRequest", order)
-                    .Error(exception, "Exception occurred during pallet insert .");
-                await Task.FromException(exception);
-            }
-        }
 
         public async Task AddOrModifyPallet(PalletDetails palletDetailsRequest)
         {
