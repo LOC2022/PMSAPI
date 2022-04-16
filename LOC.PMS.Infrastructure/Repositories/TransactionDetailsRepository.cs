aa3d893f06b4bb523ef4a82f057c26512a3259c1
pratheshpgp <45036226+pratheshpgp@users.noreply.github.com>
2022-04-16 10:27:17 +0530

    commit skelton

---
+++ LOC.PMS.Infrastructure/Repositories/TransactionDetailsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOC.PMS.Infrastructure.Repositories
+{
+    internal class TransactionDetailsRepository
+    {
+    }
+}
