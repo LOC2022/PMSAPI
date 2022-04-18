64faefbd12fd23623c17921bc7078b398b732d83
pratheshpgp <45036226+pratheshpgp@users.noreply.github.com>
2022-04-18 09:34:26 +0530

    add vendorId to seperate the dispatch dc for caterpillar

--- LOC.PMS.Application/Interfaces/ITransactionDetailsProvider.cs
+++ LOC.PMS.Application/Interfaces/ITransactionDetailsProvider.cs
@@ -10,7 +10,7 @@ namespace LOC.PMS.Application.Interfaces
     {
         Task<IEnumerable<OrderDetails>> GetHHTOrderDetails(OrderDetails orderDetails);
         Task<IEnumerable<DCDetails>> GetDCDetails(string orderNo, string DCStatus, string UserName);
-        Task UpdateScanDetails(List<int> PalletIds, int ScannedQty, string ToStatus, string OrderNumber);
+        Task UpdateScanDetails(List<int> PalletIds, int ScannedQty, string ToStatus, string OrderNumber, int VendorId);
         Task SaveVehicleAndUpdateStatus(VechicleDetails vechicleDetails, string ToDCStage, string ToPalletStage);
         Task SaveHHTOrderDetails(List<OrderDetails> orderDetails);
     }
